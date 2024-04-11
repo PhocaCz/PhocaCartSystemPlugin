6ed749c3f41e9301070cb1bc978cf46e65986a00
n3t <n3t@n3t.cz>
2024-04-11 02:20:39 +0200

    Support for dynamic titles in com_mails

--- src/Extension/PhocaCart.php
+++ src/Extension/PhocaCart.php
@@ -12,9 +12,12 @@
 namespace Joomla\Plugin\System\PhocaCart\Extension;
 
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Menu\AdministratorMenuItem;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Uri\Uri;
+use Joomla\Database\DatabaseInterface;
 use Phoca\PhocaCart\User\AdvancedACL;
 
 // phpcs:disable PSR1.Files.SideEffects
@@ -51,7 +54,7 @@ final class PhocaCart extends CMSPlugin
 		 * @var int $index
 		 * @var AdministratorMenuItem $item
 		 */
-		foreach ($items as $index => $item) {
+		foreach ($items as $item) {
 			if ($item->component_id !== $component->id) {
 				continue;
 			}
@@ -71,6 +74,43 @@ final class PhocaCart extends CMSPlugin
 				$item->getParent()->removeChild($item);
 			}
 		}
+	}
+
+	public function onAfterRoute()
+	{
+		$app = $this->getApplication();
+
+		if (!$app->isClient('administrator')) {
+			return;
+		}
+
+		if ($app->getInput()->getCmd('option') != 'com_mails') {
+			return;
+		}
+
+		$language = $app->getLanguage();
+		$language->load('com_phocacart');
+
+		$strings = [];
+
+		$db = Factory::getDbo();
+		$query = $db->getQuery(true)
+			->select('id, title')
+			->from('#__phocacart_order_statuses');
+		$db->setQuery($query);
+		$statuses = $db->loadObjectList();
+
+		foreach ($statuses as $status) {
+			$strings['COM_PHOCACART_MAIL_ORDER_STATUS_' . $status->id . '_TITLE'] = Text::_('COM_PHOCACART_MAIL_ORDER_STATUS_TITLE') . ' - ' . Text::_($status->title);
+			$strings['COM_PHOCACART_MAIL_ORDER_STATUS_' . $status->id . '_DESC'] = Text::_('COM_PHOCACART_MAIL_ORDER_STATUS_DESC');
+			$strings['COM_PHOCACART_MAIL_OTHERS_ORDER_STATUS_' . $status->id . '_TITLE'] = Text::_('COM_PHOCACART_MAIL_OTHERS_ORDER_STATUS_TITLE') . ' - ' . Text::_($status->title);
+		}
+
+		$closure = \Closure::bind(function (array $strings) {
+			$this->strings = array_replace($this->strings, $strings, $this->override);
+		}, $language, get_class($language));
+
+		$closure($strings);
 	}
 
 }
